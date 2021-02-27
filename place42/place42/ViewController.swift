8d0a59dfd70e66e4a080907bb616e98ae2362a9d
ChoiKanghun <rkdgns4562@naver.com>
2021-02-27 21:56:56 +0900

    place42 레포 생성

---
+++ place42/place42/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  place42
+//
+//  Created by 최강훈 on 2021/02/26.
+//  Copyright © 2021 최강훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
