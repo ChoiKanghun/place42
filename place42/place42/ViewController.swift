94d4dc3862663914845eaf1091054ce0da74a1bb
ChoiKanghun <rkdgns4562@naver.com>
2021-03-01 21:56:09 +0900

    지도에 내 주소 출력

--- place42/place42/ViewController.swift
+++ place42/place42/ViewController.swift
@@ -12,11 +12,13 @@ import GoogleMaps
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         // 해당 컨트롤러를 locationManager의 delegate으로 설정.
         locationManager.delegate = self
@@ -32,10 +34,46 @@ class ViewController: UIViewController {
             // 위치 정보 사용에 동의하지 않은 경우 다시 요청.
             locationManager.requestWhenInUseAuthorization()
         }
+        
+        mapView.delegate = self
+
     }
 
-    
+    func reverseGeocode(coordinate: CLLocationCoordinate2D) {
+        // geocoder는 위, 경도를 street 주소로 바꾸어준다.
+        let geocoder = GMSGeocoder()
+        
+        // geocoder에게 coordinate(위, 경도 좌표)를 street 주소로 변환하라고 요청.
+        // response는 변환된 주소.
+        geocoder.reverseGeocodeCoordinate(coordinate) {
+            response, error in
+            guard let address = response?.firstResult(),
+                let lines = address.lines
+                else {return}
+            
+            // 글자를 set.
+            // joined는 개행으로 구분된 문자열을 한 줄로 붙여줌.
+            self.addressLabel.text = lines.joined(separator: "\n")
+            
+            let labelHeight = self.addressLabel.intrinsicContentSize.height
+            let topInset = self.view.safeAreaInsets.top
+            
+            self.mapView.padding = UIEdgeInsets(
+                top: topInset,
+                left: 0,
+                bottom: labelHeight,
+                right: 0)
+            
+            // 주소의 변화가 있다면 설정한 duration에 걸쳐 animate.
+            UIView.animate(withDuration: 0.25) {
+                
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
 }
+
+
 
 extension ViewController: CLLocationManagerDelegate {
     // location permission이 승인될 때 해당 함수가 실행됨
@@ -72,5 +110,17 @@ extension ViewController: CLLocationManagerDelegate {
     // 에러 처리.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print (error)
+    }
+}
+
+extension ViewController: GMSMapViewDelegate {
+
+    // 좌표가 변할 때마다 실행.
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        reverseGeocode(coordinate: position.target)
+    }
+
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+
     }
 }
