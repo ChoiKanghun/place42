bf43884bdcb570bcf7eab9a30c8fdddfe1230506
ChoiKanghun <rkdgns4562@naver.com>
2021-05-11 20:09:40 +0900

    백그라운드-fetch 관련 공부 완료
    푸시 알림 원리와 앱의 라이프사이클을 미리 파악하지 않고
    단순히 백그라운드에서 어떻게 통신할지에만 몰두한 것이
    문제가 되어 결국 이틀 간 삽질한 결과를 내었지만
    그래도 목표 기간 내에 어떻게 테스트를 완전히 진행할 지
    파악하는 계기가 됨.

--- place42/place42/AppDelegate.swift
+++ place42/place42/AppDelegate.swift
@@ -73,132 +73,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         // 화면에 앱 이미지를 1초 동안 띄웁니다.
         Thread.sleep(forTimeInterval: 1.0)
         
-        // 앱 푸쉬 알림
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
-            
-        } else {
-            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        Messaging.messaging().delegate = self
-    
-        // 앱 푸시 알림 끝
-        
-        // 백그라운드 fetch 설정
-        
-        
-//        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.kchoi.place42.test", using: nil) { (task) in
-//
-//            self.scheduleAppRefresh()
-//
-//            let queue = OperationQueue()
-//            queue.maxConcurrentOperationCount = 1
-//            queue.addOperation {
-//                self.fetchCount += 1
-//                print(self.fetchCount)
-//            }
-//            task.setTaskCompleted(success: true)
-//
-//        }
-        registerBackgroundTasks()
+ 
         return true
     }
-    
-    
-// MARK:- background tasks
-    func registerBackgroundTasks() {
-        if #available(iOS 13.0 , *) {
-            //Register the BGAppRereshTask
-            print("Entered to registering tasks function")
-            
-            BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.kchoi.place42.refresh", using: nil) { task in
-                self.handleAppRefresh(task: task as! BGAppRefreshTask)
-            }
-            
-            BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.kchoi.place42.dataprocessing", using: nil) { task in
-                self.handleDataProcessing(task: task as! BGProcessingTask)
-            }
-        }
-    }
-    
-    func scheduleAppRefresh() {
-        print("Entered to scheduleAppRefresh")
-        let request = BGAppRefreshTaskRequest(identifier: "com.kchoi.place42.refresh")
-        request.earliestBeginDate = Date(timeIntervalSinceNow: 1 * 60)
-        
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("Couldn't schedule app refresh : \(error)")
-        }
-    }
-    
-    func scheduleDataProcessing() {
-        print("Entered to dataprocessing")
-
-        let request = BGProcessingTaskRequest(identifier: "com.kchoi.place42.dataprocessing")
-        request.requiresNetworkConnectivity = true
-        // request.requiresExternalPower = true
-        
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("couldn't schedule database cleaning : \(error)")
-        }
-    }
-    
-    func handleAppRefresh(task: BGAppRefreshTask) {
-        scheduleAppRefresh()
-        
-        var completionStatus = true
-        print("handler App Refresh called")
-        
-        task.expirationHandler = {
-            print("handler app refresh done")
-        }
-        task.setTaskCompleted(success: completionStatus)
-    }
-    
-    func handleDataProcessing(task: BGProcessingTask) {
-        var completionStatus = true
-        
-        print("handler dataprocessing called")
-
-        task.expirationHandler = {
-            print("handleDataProcessing done")
-        }
-        
-        task.setTaskCompleted(success: completionStatus)
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        print("Application Did Enter Background")
-        scheduleAppRefresh()
-        scheduleDataProcessing()
-    }
-//    func applicationDidEnterBackground(_ application: UIApplication) {
-//        scheduleAppRefresh()
-//    }
-//
-//    func scheduleAppRefresh() {
-//        let request = BGAppRefreshTaskRequest(identifier: "com.kchoi.place42.test")
-//
-//        request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
-//
-//        do {
-//            try BGTaskScheduler.shared.submit(request)
-//        } catch {
-//            print("Couldn't schedule app refresh: \(error)")
-//        }
-//    }
-//
+ 
     
     // 앱 푸시 등록 실패시
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
@@ -207,7 +85,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     // 앱 푸시 등록 성공 시
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
-        /*클라우드 푸시 등록 성공 시*/
         
         /* 로컬 푸시 등록 성공 시
         let tokenParts = deviceToken.map{data in String(format:"%02.2hhx", data)}
@@ -245,26 +122,5 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     }
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .badge, .sound])
-    }
-}
-
-// MARK:- 메시징 FIREBASE
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        
-        Messaging.messaging().token {
-            (token, error) in
-            if let error = error {
-                print("Error fetching FCM registration token: \(error)")
-            } else if let token = token {
-                print("FCM registration token: \(token)")
-            
-            }
-        }
-        
-        print("Firebase registration token: \(String(describing: fcmToken))")
-        
-        let dataDict: [String: String] = ["token": fcmToken ?? ""]
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
     }
 }
